4b4f1977701a218bf07bcf646d00ae757df31260
woodburydev <54697770+woodburydev@users.noreply.github.com>
2021-04-08 09:39:11 -0700

    Revert "SAD"
    This reverts commit bc223a8e5e9f668328e1bc77921400ab5f458420.

--- src/Components/Chat/LoadingPage/LoadingPage.tsx
+++ src/Components/Chat/LoadingPage/LoadingPage.tsx
@@ -1,50 +1,10 @@
 import React from "react";
 import { Logo } from "Theme/misc";
+import { IsTyping } from "../Chat/Chat-css";
 import { LoadingContainer, LogoContainer } from "./LoadingPage-css";
 import { MainContainer } from "Theme/containers";
-import styled, { css, keyframes } from "styled-components";
 
 export default function LoadingPage({ propStyles = undefined }: any) {
-  const loadingFade = keyframes`
-0% {
-  opacity: 0;
-}
-50% {
-  opacity: 0.8;
-}
-100% {
-  opacity: 0;
-}
-`;
-
-  const IsTypingCSS = css`
-    will-change: transform;
-    span {
-      height: 10px;
-      width: 10px;
-      float: left;
-      margin: 0 1px;
-      background-color: ${(props) => props.theme.fontColors.defaultMediumGrey};
-      display: block;
-      border-radius: 50%;
-      opacity: 0.4;
-      animation: ${loadingFade} 1s infinite;
-    }
-    span:nth-of-type(1) {
-      animation-delay: 0s;
-    }
-    span:nth-of-type(2) {
-      animation-delay: 0.2s;
-    }
-    span:nth-of-type(3) {
-      animation-delay: 0.4s;
-    }
-  `;
-
-  const IsTyping = styled.div`
-    ${IsTypingCSS}
-  `;
-
   return (
     <MainContainer style={propStyles ? propStyles : undefined}>
       <LogoContainer>
